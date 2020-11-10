7b46c909201194f3ee0d0d0e0980529a4c03f8bb
Nitin <nitinvardha09@gmail.com>
2020-11-11 01:37:17 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Add from './components/Add';
 import Todos from './components/Todos';
 import Navbar from './components/Navbar';
-// import './App.css';
+
 
 function App() {
 
